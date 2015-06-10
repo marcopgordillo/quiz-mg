a4982d90eb2fcab51125fad5c148bbd4e734cdd7
Marco Gordillo <marcopgordillo@gmail.com>
2015-06-10 23:15:37 +0000

    añadido postgress

--- models/models.js
+++ models/models.js
@@ -1,11 +1,31 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgress://user:password@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name 	= (url[6]||null);
+var user 		= (url[2]||null);
+var pwd 		= (url[3]||null);
+var protocol 	= (url[1]||null);
+var dialect		= (url[1]||null);
+var port 		= (url[5]||null);
+var host	 	= (url[4]||null);
+var storage 	= process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-		{dialect: 'sqlite', storage: 'quiz.sqlite'}
+//var sequelize = new Sequelize(null, null, null,
+var sequelize = new Sequelize(DB_name, user, pwd,
+		{	dialect: 	dialect,
+			protocol: 	protocol,
+			port: 		port,
+			host: 		host,
+			storage: 	storage, 	// Solo SQLite (.env)
+			omitNull: 	true		// Solo Postgress
+		}
 	);
 
 // Importar  y exportar la definición de la tabla Quiz en quiz.js
@@ -13,16 +33,6 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en BDD
-/*sequelize.sync().success(function () {
-	Quiz.count().success(function (count) {
-		if (count === 0) { // La tabla se inicializa solo si está vacía.
-			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'})
-				.success(function () {
-					console.log('Base de datos inicializada');
-				});
-		}
-	});
-});*/
 
 Quiz.sync({force: true}).then(function () {
   // Table created
