8ee434c8a2829fa0dacb3a729997ffce90216904
Marco Gordillo <marcopgordillo@gmail.com>
2015-06-13 08:01:32 +0000

    Añadido /POST /quizes/create

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,9 +11,7 @@ exports.load = function (req, res, next, quizId) {
 						next(new Error('No existe QuizId' + quizId));
 					}
 				})
-				.catch(function (error) {
-					next(error);
-				});
+				.catch(function (error) { next(error); });
 };
 
 
@@ -26,11 +24,11 @@ exports.index = function (req, res) {
 			 order:[['pregunta', 'ASC']]}
 		).then(function (quizes) { // Se recupera todas las preguntas que coinciden la búsqueda
 			res.render('quizes/index.ejs', {quizes: quizes, title: 'Preguntas'});
-		});
+		}).catch(function(error) { next(error);});
 	} else{
 		models.Quiz.findAll().then(function (quizes) { // Se recupera todas las preguntas
 			res.render('quizes/index.ejs', {quizes: quizes, title: 'Preguntas'});
-		});
+		}).catch(function(error) { next(error);});
 	}
 
 	
@@ -48,4 +46,23 @@ exports.answer = function (req, res) {
 		resultado = 'Correcto';
 		
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, title: 'Respuesta'});	
+};
+
+// GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build( // crea el objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+			
+	res.render('quizes/new', {quiz: quiz, title: 'Respuesta'});	
+};
+
+// POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields:['pregunta', 'respuesta']}).then(function () {
+		res.redirect('/quizes');	// Redirección HTTP (URL relativo) a lista de preguntas
+	}).catch(function(error) { next(error);});	
 };
